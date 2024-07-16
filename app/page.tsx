40be82c3d6035c9ea0bface35640796e40408e26
adrian729 <adrian.sanchez.albanell@gmail.com>
2024-07-16 21:59:12 +0100

    Create base next app

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main>
+            <h1>Ejercicios alemán</h1>
+            <p>Ejercicios para aprender alemán</p>
+        </main>
+    );
+}
