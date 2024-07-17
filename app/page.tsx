25774b8aa9eb44e85acf074737cd0b6ad8709c01
adrian729 <adrian.sanchez.albanell@gmail.com>
2024-07-17 18:40:35 +0100

    Update prisma schema and migrations, added test page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,161 @@
-export default function Home() {
+import type { WordType } from "@/data/words";
+import { words } from "@/data/words";
+import type { Category, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function getWordsCategories(words: WordType[]): string[] {
+    return words.reduce<string[]>((acc, word) => {
+        word.categories.forEach((category) => {
+            if (!acc.includes(category)) {
+                acc.push(category);
+            }
+        });
+        return acc;
+    }, []);
+}
+
+async function upsertCategories(categories: string[]): Promise<Category[]> {
+    return Promise.all(
+        categories.map(async (category) => {
+            const record = await prisma.category.upsert({
+                where: {
+                    name: category,
+                },
+                update: {},
+                create: {
+                    name: category,
+                },
+            });
+            return record;
+        })
+    );
+}
+
+type WordWithCategories = Prisma.WordGetPayload<{
+    include: {
+        categories: true;
+    };
+}>;
+
+async function upsertWords(words: WordType[]): Promise<WordWithCategories[]> {
+    return Promise.all(
+        words.map(async (word) => {
+            const {
+                german,
+                pronunciation,
+                spanish,
+                categories,
+                invAsociation,
+            } = word;
+
+            const record = await prisma.word.upsert({
+                where: {
+                    translationId: {
+                        german: word.german,
+                        spanish: word.spanish,
+                    },
+                },
+                update: {
+                    german,
+                    pronunciation,
+                    spanish,
+                    invAsociation,
+                    categories: {
+                        connectOrCreate: categories.map((category) => ({
+                            where: { name: category },
+                            create: { name: category },
+                        })),
+                    },
+                },
+                create: {
+                    german,
+                    pronunciation,
+                    spanish,
+                    invAsociation,
+                    categories: {
+                        connectOrCreate: categories.map((category) => ({
+                            where: { name: category },
+                            create: { name: category },
+                        })),
+                    },
+                },
+                include: {
+                    categories: true,
+                },
+            });
+            return record;
+        })
+    );
+}
+
+async function populateWords(): Promise<[Category[], WordWithCategories[]]> {
+    const categories = await getWordsCategories(words);
+    const populatedCategories = await upsertCategories(categories);
+    const populatedWords = await upsertWords(words);
+    return [populatedCategories, populatedWords];
+}
+
+async function getCategories(): Promise<Category[]> {
+    const categoriesToUpsert = [
+        {
+            name: "Animales",
+        },
+        {
+            name: "Naturaleza",
+        },
+        {
+            name: "Sustantivo",
+        },
+    ];
+
+    return Promise.all(
+        categoriesToUpsert.map(async (category) => {
+            const record = await prisma.category.upsert({
+                where: {
+                    name: category.name,
+                },
+                update: {},
+                create: category,
+            });
+            return record;
+        })
+    );
+}
+
+export default async function Home() {
+    const [populatedCategories, populatedWords] = await populateWords();
+
     return (
-        <main>
+        <main className="m-20">
             <h1>Ejercicios alemán</h1>
             <p>Ejercicios para aprender alemán</p>
+
+            <div className="border border-black flex flex-col items-center justify-center">
+                <h2 className="font-bold text-lg">Categories</h2>
+                <ul>
+                    {populatedCategories.map((category) => (
+                        <li key={category.id}>{category.name}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="border border-black flex flex-col items-center justify-center">
+                <h2 className="font-bold text-lg">Words</h2>
+                <ul>
+                    {populatedWords.map((word) => (
+                        <li key={word.id} className="border border-red-500">
+                            {word.german} - {word.spanish}
+                            <ul>
+                                {word.categories.map(({ id, name }) => (
+                                    <li key={id}>{name}</li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </main>
     );
 }
---
+++ data/words.ts
@@ -0,0 +1,167 @@
+export type WordType = {
+    german: string;
+    pronunciation?: string;
+    spanish: string;
+    invAsociation?: string;
+    categories: string[];
+};
+
+export const other: WordType[] = [];
+
+export const pronouns: WordType[] = [
+    {
+        german: "Wir",
+        pronunciation: "vía",
+        spanish: "Nosotros",
+        categories: ["Pronombre"],
+        invAsociation:
+            "Imagina que NOSOTROS estamos ahora mismo sentados en la VÍA del tren",
+    },
+    {
+        german: "Sie",
+        pronunciation: "si:",
+        spanish: "Usted(es)/Ell(os/as)",
+        categories: ["Pronombre"],
+        invAsociation: "¡Pero SI Es USTED!",
+    },
+];
+
+export const adjectives: WordType[] = [];
+
+export const adverbs: WordType[] = [
+    {
+        german: "Ja",
+        pronunciation: "ya",
+        spanish: "Si",
+        categories: ["Adverbio"],
+    },
+    {
+        german: "Nein",
+        pronunciation: "náin",
+        spanish: "No",
+        categories: ["Adverbio"],
+    },
+    {
+        german: "Nicht",
+        pronunciation: "nísht",
+        spanish: "NEGATIVA (negar una frase)",
+        categories: ["Adverbio"],
+    },
+];
+
+export const verbs: WordType[] = [
+    {
+        german: "Haben",
+        pronunciation: "háben",
+        spanish: "Haber/Tener",
+        categories: ["Verbo"],
+    },
+    {
+        german: "Wollen",
+        pronunciation: "vólen",
+        spanish: "Querer",
+        categories: ["Verbo"],
+        invAsociation: "QUERER VOLAR",
+    },
+    {
+        german: "Können",
+        pronunciation: "k<u>oé</u>nen",
+        spanish: "Poder",
+        categories: ["Verbo"],
+        invAsociation: "CONAN el bárbaro tenía mucho PODER",
+    },
+    {
+        german: "Arbeiten",
+        pronunciation: "árbaiten",
+        spanish: "Trabajar",
+        categories: ["Verbo"],
+        invAsociation:
+            "Vemos TRABAJAR velozmente a un ÁRBOL con muchas ramas a modo de brazos",
+    },
+    {
+        german: "Essen",
+        pronunciation: "ésen",
+        spanish: "Comer",
+        categories: ["Verbo"],
+        invAsociation:
+            "A más COMER más forma de ESE crea el cuerpo (echa barriga y culo)",
+    },
+    {
+        german: "Lessen",
+        pronunciation: "lí:sen",
+        spanish: "Leer",
+        categories: ["Verbo"],
+        invAsociation: "Un LESIONADO tiene más tiempo libre para LEER",
+    },
+    {
+        german: "Studieren",
+        pronunciation: "shtudíren",
+        spanish: "Estudiar",
+        categories: ["Verbo"],
+    },
+    {
+        german: "Kaufen",
+        pronunciation: "kaúfen",
+        spanish: "Comprar",
+        categories: ["Verbo"],
+        invAsociation:
+            "Queda K.O. EL FEO boceador al no poder seguir el ritmo de su novia COMPRANDO",
+    },
+    {
+        german: "Mieten",
+        pronunciation: "mí:ten",
+        spanish: "Alquilar",
+        categories: ["Verbo"],
+        invAsociation: "Cuando ALQUILAS algo parece que es MITAD tuyo",
+    },
+];
+
+export const nouns: WordType[] = [
+    {
+        german: "Auto",
+        pronunciation: "áuto",
+        spanish: "Auto/Coche",
+        categories: ["Sustantivo"],
+        invAsociation: "AUTOmóvil",
+    },
+    {
+        german: "Zimmer",
+        pronunciation: "tsíma",
+        spanish: "Habitación",
+        categories: ["Sustantivo"],
+        invAsociation:
+            "Una HABITACIÓN situada en equilibrio en la CIMA de una montaña",
+    },
+    {
+        german: "Buch",
+        pronunciation: "buj",
+        spanish: "Libro",
+        categories: ["Sustantivo"],
+        invAsociation:
+            "Casi como en inglés BOOK / Un ave muy culta, con gafas, está emBUCHando un LIBRO",
+    },
+    {
+        german: "Arbeit",
+        pronunciation: "árbait",
+        spanish: "Trabajo",
+        categories: ["Sustantivo"],
+        invAsociation:
+            "(Trabajar = ARBEITen) / Un ARBOL con muchas ramas a modo de brazos TRABAJA velozmente",
+    },
+    {
+        german: "Brot",
+        pronunciation: "brot",
+        spanish: "Pan",
+        categories: ["Sustantivo"],
+        invAsociation: "Vemos BROTar PAN de una planta",
+    },
+];
+
+export const words: WordType[] = [
+    ...verbs,
+    ...nouns,
+    ...adjectives,
+    ...adverbs,
+    ...pronouns,
+    ...other,
+];
